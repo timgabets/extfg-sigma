b4cffc0e5ace4999fe1b9a3f7b3784e0141678ba
Tim Gabets <t.gabets@tinkoff.ru>
2020-06-30 12:53:39 +0300

    Tags

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 use serde_json::Value;
 use std::io;
 
-//use std::collections::BTreeMap;
+use std::collections::BTreeMap;
 
 mod util;
 
@@ -12,7 +12,7 @@ pub struct SigmaRequest {
     source: String,
     mti: String,
     auth_serno: u64,
-    //tags: BTreeMap<String, String>,
+    tags: BTreeMap<usize, String>,
     //iso_fields: BTreeMap<String, String>,
 }
 
@@ -23,6 +23,7 @@ impl SigmaRequest {
             source: String::from("X"),
             mti: String::from("0100"),
             auth_serno: 0,
+            tags: BTreeMap::new(),
         };
 
         // SAF
@@ -85,7 +86,7 @@ impl SigmaRequest {
             }
         }
 
-        // Source
+        // Authorization serno
         f = "Serno";
         match data.get(f) {
             Some(opt) => match opt.as_u64() {
@@ -104,6 +105,31 @@ impl SigmaRequest {
                 req.auth_serno = util::gen_auth_serno();
             }
         }
+
+        // Tags
+        for i in 0..23 {
+            let id = format!("T{:04}", i);
+            match data.get(id) {
+                Some(tag) => match tag.as_str() {
+                    Some(v) => {
+                        req.tags.insert(i, v.to_string());
+                    }
+                    None => match tag.as_u64() {
+                        Some(v) => {
+                            req.tags.insert(i, v.to_string());
+                        }
+                        None => {
+                            println!("Incoming request has invalid {} field data format - should be either String or u64", format!("T{:04}", i));
+                            return Err(io::ErrorKind::InvalidData);
+                        }
+                    },
+                },
+                None => {}
+            }
+        }
+
+        // ISO Fields
+        
 
         Ok(req)
     }
@@ -124,22 +150,22 @@ mod tests {
             "SRC": "M",
             "MTI": "0200",
             "Serno": 6007040979,
-            "T0000": "02371492071643",
+            "T0000": 2371492071643,
             "T0001": "C",
-            "T0002": "643",
+            "T0002": 643,
             "T0003": "000100000000",
-            "T0004": "643",
-            "T0005": "000100000000",
+            "T0004": 978,
+            "T0005": "000300000000",
             "T0006": "OPS6",
-            "T0007": "19",
-            "T0008": "643",
-            "T0009": "3104",
-            "T0010": "3104",
-            "T0011": "2",
+            "T0007": 19,
+            "T0008": 643,
+            "T0009": 3102,
+            "T0010": 3104,
+            "T0011": 2,
             "T0014": "IDDQD Bank",
-            "T0016": "74707182",
+            "T0016": 74707182,
             "T0018": "Y",
-            "T0022": "000000000000"
+            "T0022": "000000000010"
         }"#;
 
         let r: SigmaRequest = SigmaRequest::new(serde_json::from_str(&payload).unwrap()).unwrap();
@@ -147,6 +173,43 @@ mod tests {
         assert_eq!(r.source, "M");
         assert_eq!(r.mti, "0200");
         assert_eq!(r.auth_serno, 6007040979);
+        assert_eq!(r.tags.get(&0).unwrap(), "2371492071643");
+        assert_eq!(r.tags.get(&1).unwrap(), "C");
+        assert_eq!(r.tags.get(&2).unwrap(), "643");
+        assert_eq!(r.tags.get(&3).unwrap(), "000100000000");
+        assert_eq!(r.tags.get(&4).unwrap(), "978");
+        assert_eq!(r.tags.get(&5).unwrap(), "000300000000");
+        assert_eq!(r.tags.get(&6).unwrap(), "OPS6");
+        assert_eq!(r.tags.get(&7).unwrap(), "19");
+        assert_eq!(r.tags.get(&8).unwrap(), "643");
+        assert_eq!(r.tags.get(&9).unwrap(), "3102");
+        assert_eq!(r.tags.get(&10).unwrap(), "3104");
+        assert_eq!(r.tags.get(&11).unwrap(), "2");
+
+        match r.tags.get(&12) {
+            Some(_) => assert!(false),
+            None => assert!(true),
+        }
+
+        match r.tags.get(&13) {
+            Some(_) => assert!(false),
+            None => assert!(true),
+        }
+
+        assert_eq!(r.tags.get(&14).unwrap(), "IDDQD Bank");
+
+        match r.tags.get(&15) {
+            Some(_) => assert!(false),
+            None => assert!(true),
+        }
+
+        assert_eq!(r.tags.get(&16).unwrap(), "74707182");
+        match r.tags.get(&17) {
+            Some(_) => assert!(false),
+            None => assert!(true),
+        }
+        assert_eq!(r.tags.get(&18).unwrap(), "Y");
+        assert_eq!(r.tags.get(&22).unwrap(), "000000000010");
     }
 
     #[test]
